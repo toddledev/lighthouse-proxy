f3577e72a468d0df67a1814f9727dcb023c793cd
Erik Beuschau <bager.erik@gmail.com>
2024-10-10 09:17:27 +0200

    Initial commit for lighthouse proxy

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
-import { fromHono } from "chanfana";
-import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { secureHeaders } from 'hono/secure-headers'
+import { lighthouse } from 'lighthouse'
 
-// Start a Hono app
-const app = new Hono();
+const app = new Hono()
+app.use(secureHeaders())
+app.use('/api/*', cors())
+app.post('/api/lighthouse', lighthouse)
 
-// Setup OpenAPI registry
-const openapi = fromHono(app, {
-	docs_url: "/",
-});
-
-// Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
-
-// Export the Hono app
-export default app;
+export default app
---
+++ src/lighthouse.ts
@@ -0,0 +1,86 @@
+import { Ratelimit } from '@upstash/ratelimit' // for deno: see above
+import { Redis } from '@upstash/redis/cloudflare'
+import { Context } from 'hono'
+import type { Env } from './worker-configuration'
+
+// The maximum age of a cached value that we will serve
+const SERVE_STALE_INTERVAL = 24 * 60 * 60 * 1000 // 1 hour
+// The maximum age of a requested cached value before we rebuild it (in the background)
+const REBUILD_MAX_TTL = SERVE_STALE_INTERVAL - 5 * 60 * 1000 // 55 minutes
+
+export const lighthouse = async (ctx: Context<{ Bindings: Env }>) => {
+  let lighthouseUrl: URL
+  try {
+    const body = await ctx.req.json()
+    lighthouseUrl = new URL(body?.url)
+  } catch {
+    return Response.json({ error: 'Invalid request' }, { status: 400 })
+  }
+  const [ttl, result] = await Redis.fromEnv(ctx.env)
+    .multi()
+    .pttl(lighthouseUrl.href)
+    .get<Record<string, any>>(lighthouseUrl.href)
+    .exec()
+  if (result && ttl > 0) {
+    if (ttl < REBUILD_MAX_TTL) {
+      // Rebuild cache in the background
+      console.log('Rebuilding cache (in background) for', lighthouseUrl.href)
+      ctx.executionCtx.waitUntil(callLighthouse(lighthouseUrl.href, ctx, true))
+    }
+    return Response.json(result)
+  }
+  // Create a new ratelimiter, that allows 5 requests per 60 seconds per ip address
+  const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(ctx.env),
+    limiter: Ratelimit.slidingWindow(5, '60 s'),
+    analytics: true,
+    prefix: 'RATELIMIT:',
+  })
+  const ip =
+    ctx.req.raw.headers.get('x-real-ip') ??
+    ctx.req.raw.headers.get('cf-connecting-ip')
+  const { success } = await ratelimit.limit(ip)
+  if (!success) {
+    return Response.json({ error: 'Rate limit exceeded' }, { status: 429 })
+  }
+  const data = await callLighthouse(lighthouseUrl.href, ctx, false)
+  if (data) {
+    return Response.json(data)
+  }
+  return Response.json(
+    { error: 'Failed to fetch Lighthouse score' },
+    { status: 500 },
+  )
+}
+
+const callLighthouse = async (
+  url: string,
+  ctx: Context<{ Bindings: Env }>,
+  background: boolean,
+) => {
+  console.log('Calling Lighthouse for url:', url)
+  if (background) {
+    // Check if we started a background lighthouse request less than 30 seconds ago for this url
+    const lastRequest = await Redis.fromEnv(ctx.env).get('lh:' + url)
+    if (lastRequest === true) {
+      console.log('Skipping Lighthouse request for url: ', url)
+      return
+    }
+  }
+  // Save a record that we started a lighthouse request for this url
+  await Redis.fromEnv(ctx.env).set('lh:' + url, true, { px: 30 * 1000 })
+  const response = await fetch(
+    `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?key=${ctx.env.PAGESPEED_API_KEY}&category=performance&url=${url}`,
+  )
+  try {
+    const data = await response.json()
+    // Persist the result to Redis cache in the background
+    console.log('Caching result for url: ', url)
+    ctx.executionCtx.waitUntil(
+      Redis.fromEnv(ctx.env).set(url, data, { px: SERVE_STALE_INTERVAL }),
+    )
+    return data
+  } catch {
+    return
+  }
+}
